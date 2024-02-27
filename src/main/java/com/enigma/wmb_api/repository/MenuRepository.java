5ec02db954a0ae8be008d2a14aa5b670d8367191
M. Shadiq Firmansyah Basiru <shodiqbasiru@gmail.com>
2024-02-28 00:03:14 +0700

    [IMPROVE] Add Annotation for Repository

---
+++ src/main/java/com/enigma/wmb_api/repository/MenuRepository.java
@@ -0,0 +1,9 @@
+package com.enigma.wmb_api.repository;
+
+import com.enigma.wmb_api.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu,String> {
+}
