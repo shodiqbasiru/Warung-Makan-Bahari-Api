73694abaaf5663152720b64dc3727c16187b850c
M. Shadiq Firmansyah Basiru <shodiqbasiru@gmail.com>
2024-02-29 18:52:34 +0700

    [IMPROVE] make method for validation constraint violation excepetion

--- src/main/java/com/enigma/wmb_api/controller/error/ErrorController.java
+++ src/main/java/com/enigma/wmb_api/controller/error/ErrorController.java
@@ -1,6 +1,8 @@
 package com.enigma.wmb_api.controller.error;
 
 import com.enigma.wmb_api.dto.response.CommonResponse;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,5 +17,14 @@ public class ErrorController {
                 .message(exception.getReason())
                 .build();
         return new ResponseEntity<>(response, exception.getStatusCode());
+    }
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    public ResponseEntity<CommonResponse<?>> constraintViolationExceptionHandler(ConstraintViolationException e){
+        CommonResponse<?> response =CommonResponse.builder()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .message(e.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ src/main/java/com/enigma/wmb_api/util/ValidationUtil.java
@@ -0,0 +1,4 @@
+package com.enigma.wmb_api.util;
+
+public class ValidationUtil {
+}
