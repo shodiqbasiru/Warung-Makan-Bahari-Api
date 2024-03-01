4b51d2c3e3bd1549a52c408ccce6d231a8e65430
M. Shadiq Firmansyah Basiru <shodiqbasiru@gmail.com>
2024-03-01 22:44:30 +0700

    [FEATURED] make a feat AuthController for login and register

--- src/main/java/com/enigma/wmb_api/controller/AuthController.java
+++ src/main/java/com/enigma/wmb_api/controller/AuthController.java
@@ -1,4 +1,53 @@
 package com.enigma.wmb_api.controller;
 
+import com.enigma.wmb_api.constant.RouteApi;
+import com.enigma.wmb_api.dto.request.AuthRequest;
+import com.enigma.wmb_api.dto.response.CommonResponse;
+import com.enigma.wmb_api.dto.response.LoginResponse;
+import com.enigma.wmb_api.dto.response.RegisterResponse;
+import com.enigma.wmb_api.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(RouteApi.AUTH_API)
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping(
+            path = "/register",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<CommonResponse<?>> register(@RequestBody AuthRequest request) {
+        RegisterResponse registerResponse = authService.register(request);
+        CommonResponse<RegisterResponse> response = CommonResponse.<RegisterResponse>builder()
+                .statusCode(HttpStatus.CREATED.value())
+                .message("Created a new account successfully")
+                .data(registerResponse)
+                .build();
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
+    }
+
+    @PostMapping(
+            path = "/login",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<CommonResponse<?>> login(@RequestBody AuthRequest request) {
+        LoginResponse loginResponse = authService.login(request);
+        CommonResponse<LoginResponse> response = CommonResponse.<LoginResponse>builder()
+                .statusCode(HttpStatus.CREATED.value())
+                .message("Created a new account successfully")
+                .data(loginResponse)
+                .build();
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
+    }
 }
