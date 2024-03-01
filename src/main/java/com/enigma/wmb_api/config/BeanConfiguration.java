17e0085745bff1af3604d226cd12d96082744e90
M. Shadiq Firmansyah Basiru <shodiqbasiru@gmail.com>
2024-03-01 22:40:27 +0700

    [FEATURED] make a feature BeanConfiguration for Password Encoder

--- src/main/java/com/enigma/wmb_api/config/BeanConfiguration.java
+++ src/main/java/com/enigma/wmb_api/config/BeanConfiguration.java
@@ -1,4 +1,14 @@
 package com.enigma.wmb_api.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BeanConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
