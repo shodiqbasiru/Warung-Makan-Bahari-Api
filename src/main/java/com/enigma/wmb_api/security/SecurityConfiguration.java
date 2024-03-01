d9de31ba658b04f29bd0c55169572ef5a496537b
M. Shadiq Firmansyah Basiru <shodiqbasiru@gmail.com>
2024-03-01 22:51:39 +0700

    [FEATURED] make feat SecurityConfiguration

--- src/main/java/com/enigma/wmb_api/security/SecurityConfiguration.java
+++ src/main/java/com/enigma/wmb_api/security/SecurityConfiguration.java
@@ -1,4 +1,23 @@
 package com.enigma.wmb_api.security;
 
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> {
+                    req.dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
+                            .anyRequest().permitAll();
+                })
+                .build();
+    }
 }
