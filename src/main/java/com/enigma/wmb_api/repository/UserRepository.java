6a27be18fb3dc6497f2724a7d040a86d17dbb046
M. Shadiq Firmansyah Basiru <shodiqbasiru@gmail.com>
2024-03-01 22:50:54 +0700

    [FEATURED] make feat Repositories for Role and User

--- src/main/java/com/enigma/wmb_api/repository/UserRepository.java
+++ src/main/java/com/enigma/wmb_api/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.enigma.wmb_api.repository;
 
-public interface UserRepository {
+import com.enigma.wmb_api.entity.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserAccount, String> {
 }
