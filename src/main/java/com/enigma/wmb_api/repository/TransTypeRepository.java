8788adfea08c299d5c180f206551e7e9669836e7
M. Shadiq Firmansyah Basiru <shodiqbasiru@gmail.com>
2024-03-02 19:27:16 +0700

    [IMPROVE] deleted unncessary code

--- src/main/java/com/enigma/wmb_api/repository/TransTypeRepository.java
+++ src/main/java/com/enigma/wmb_api/repository/TransTypeRepository.java
@@ -8,7 +8,5 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface TransTypeRepository extends JpaRepository<TransType,String> {
-
-    Optional<TransType> findById(TransTypeEnum id);
+public interface TransTypeRepository extends JpaRepository<TransType,TransTypeEnum> {
 }
