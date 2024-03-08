7ac903ffdef3e72cebde8daf0664f19beb53700b
M. Shadiq Firmansyah Basiru <shodiqbasiru@gmail.com>
2024-03-09 03:47:33 +0700

    [IMPROVE] add Controller for image

--- src/main/java/com/enigma/wmb_api/controller/ImageController.java
+++ src/main/java/com/enigma/wmb_api/controller/ImageController.java
@@ -1,11 +1,28 @@
 package com.enigma.wmb_api.controller;
 
+import com.enigma.wmb_api.constant.RouteApi;
 import com.enigma.wmb_api.service.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ImageController {
     private final ImageService imageService;
+
+    @GetMapping(path = RouteApi.MENU_IMAGE_API_PATH + "{imageId}")
+    public ResponseEntity<?> download(@PathVariable String imageId) {
+        Resource resource = imageService.getById(imageId);
+        String headerValue = String.format("attachment; filename=%s", resource.getFilename());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_DISPOSITION,headerValue)
+                .body(resource);
+    }
 }
